93bc58b2fe3d876b49eede76fb6afba3ee35cbd0
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-03-09 20:05:15 +0100

    adds task 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -35,3 +35,36 @@ import '../css/03-feedback.css';
 //   emailInput.value = '';
 //   messageInput.value = '';
 // }
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', saveToLocalStorage);
+
+function saveToLocalStorage() {
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const feedbackState = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+}
+
+window.addEventListener('load', () => {
+  const feedbackState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  if (feedbackState) {
+    emailInput.value = feedbackState.email;
+    messageInput.value = feedbackState.message;
+  }
+});
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const feedbackState = { email: emailInput.value, message: messageInput.value };
+  console.log(feedbackState);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+}
